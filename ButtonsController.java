3c97e22e6b26fdce06b313e20c8ba22ed3a5f143
Cleanwell <149332447+Cleanwell@users.noreply.github.com>
2023-11-02 14:35:33 -0500

    Updated the ButtonsController with three buttons

--- ButtonsController.java
+++ ButtonsController.java
@@ -2,28 +2,23 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class ButtonsController extends JPanel
-{
+public class ButtonsController extends JPanel {
     private JPanel myPanel;
     private ObservableVM252 myModel;
 
-    private JPanel getPanel()
-    {
+    private JPanel getPanel() {
         return myPanel;
     }
 
-    private ObservableVM252 getModel()
-    {
+    private ObservableVM252 getModel() {
         return myModel;
     }
 
-    private void setPanel(JPanel other)
-    {
+    private void setPanel(JPanel other) {
         myPanel = other;
     }
 
-    private void setModel(ObservableVM252 other)
-    {
+    private void setModel(ObservableVM252 other) {
         myModel = other;
     }
 
@@ -31,19 +26,33 @@ public class ButtonsController extends JPanel
     // Constructors
     //
 
-    public ButtonsController()
-    {
+    public ButtonsController() {
         this(null);
     }
 
-    public ButtonsController(ObservableVM252 initialModel)
-    {
+    public ButtonsController(ObservableVM252 initialModel) {
         setModel(initialModel);
 
-        setPanel(new JPanel());
+        //
+        // Create buttons
+        //
 
+        JButton quitButton = new JButton("Quit");
+        JButton runButton = new JButton("Run");
+        JButton helpButton = new JButton("Help");
+
+        setPanel(new JPanel());
         getPanel().setLayout(new GridLayout(3, 3));
 
+        //
+        // Add the Buttons to the panel
+        //
+
+        getpanel().add(quitButton);
+        getpanel().add(runButton);
+        getpanel().add(helpButton);
+
         add(getPanel());
+
     }
 }
